ff90cc5d1010ee8a7075c31e7bf20c55cab35dca
Ariana Alavi <micaelaariana_alavi@hotmail.com>
2024-08-17 19:14:08 -0300

    Arreglo de los ejercicios con las correciones de pylint y mejora del código. Agregado del README

--- TP1/ej3.py
+++ TP1/ej3.py
@@ -1,5 +1,15 @@
-###Funciones
-def control_gastos(cant, valor):
+"""Este programa controla los gasto del subte en un mes."""
+def control_gastos(cant: int, valor: int) -> float:
+    """Dependiendo la cantidad de viajes que se planean hacer en un mes 
+        se llama a la función adecuada.
+    
+        Args:
+            cant (int): Cantidad de viajes en un mes.
+            valor (int): Valor máximo del pasaje.
+        
+        Returns:
+            float: el total gastado en viajes.
+    """
     if cant < 21:
         total = one_to_twenty(cant, valor)
     elif cant < 31:
@@ -10,37 +20,55 @@ def control_gastos(cant, valor):
         total = calculo_pasaje(cant, valor, 0.6)
     return total
 
-def one_to_twenty(cant, valor):
+def one_to_twenty(cant: int, valor: int) -> float:
+    """De 1 a 20 viajes no hay descuentos. El precio por viaje es el valor máximo. """
     total = valor * cant
     return total
 
-def calculo_pasaje(cant, valor, percentage):
+def calculo_pasaje(cant: int, valor: int, percentage: float)-> float:
+    """
+    Args:
+        cant (int): Cantidad de viajes en un mes.
+        valor (int): Valor máximo del pasaje.
+        percentage (float): La cantidad de descuentos que se aplica 
+                            de acuerdo a la cantidad de viajes.
+    """
     total = cant * (valor * percentage)
     return total
 
-def es_valido(dato, lim1):
+def es_valido(dato: int, lim1: int):
+    """Una generica para poder establecer limites en los input y verificarlos."""
     return dato > lim1
 
-def main():
-    cant = int(input("Ingrese la cantidad de viajes realizados: "))
-    valor = int(input("Ingrese el valor de su pasaje: $"))
+def main() -> None:
+    """Permite al usuario ingresa la cantidad de viajes y el valor máximo."""
+    while True:
+        try:
+            cant = int(input("Ingrese la cantidad de viajes realizados: "))
+            valor = int(input("Ingrese el valor de su pasaje: $"))
+            break
+        except ValueError:
+            print("Debe ingresar números enteros.")
+            continue
+
     if es_valido(cant, 0) and es_valido(valor, 0):
         total = control_gastos(cant, valor)
         print(f"El total gastado en el mes es de ${total}.")
     else:
         print("Datos no válidos.")
-    return None
 
-###Verificar comportamiento de la función
-def verificar():
+
+def verificar() -> None:
+    """Verifica el comportamiento de la función 'control_gastos'."""
     for i in range(len(test[0])):
         total = control_gastos(test[0][i], test[1][i])
         print(f"El total gastado en el mes es de ${total}.")
-    return None
 
-test = [[10, 22, 33, 50, 5, 28], [120, 150, 200, 180, 90, 130],] #0. cant de viajes, 1. valor de pasaje
+test = [[10, 22, 33, 50, 5, 28], [120, 150, 200, 180, 90, 130],]
+#0. cant de viajes, 1. valor de pasaje
 
 ###Bloque principal
 main()
 print()
 verificar()
+#End-of-file (EOF)
